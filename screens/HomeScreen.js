380e0b67d61d3fc5d43baf1def75f207addfb77d
Mohammad Anjas Ferdiansyah <anjasferdiansyah123@gmail.com>
2024-10-30 19:55:37 +0700

    fix : modify HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,7 @@ import MaterialComunityIcons from "react-native-vector-icons/MaterialCommunityIc
 const HomeScreen = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <View className="w-full h-[70%] px-10 justify-center">
+      <View className="w-full px-10 justify-center">
         <View className="flex flex-row gap-4 justify-between px-5">
           <View>
             <Text className="text-6xl font-outfitBold">
@@ -21,63 +21,63 @@ const HomeScreen = () => {
           />
         </View>
 
-        <View className="flex flex-col gap-4 flex-wrap items-center justify-between self-center my-20 px-5">
+        <View className="flex flex-col gap-4 items-center justify-between self-center my-20 px-5">
           <View className="flex flex-row gap-4 items-center justify-between w-full">
-            <View className="w-1/2 p-5 h-[150px] bg-[#78F3B5] rounded-xl flex flex-col justify-between">
-              <View>
+            <View className="w-1/2 p-5 h-[190px] bg-[#78F3B5] rounded-xl flex flex-col justify-between">
+              <View className="p-4 bg-white rounded-full max-w-[60px]">
                 <MaterialComunityIcons name="cart" size={30} />
               </View>
 
               <View>
                 <Text className="text-white font-outfitBold text-2xl text-wrap">
-                  New
+                  Make
                 </Text>
                 <Text className="text-white font-outfitBold text-4xl text-wrap">
-                  Product
+                  Event
                 </Text>
               </View>
             </View>
-            <View className="w-1/2 p-5 h-[150px] bg-[#00F279] rounded-xl flex flex-col justify-between">
-              <View>
+            <View className="w-1/2 p-5 h-[190px] bg-[#00F279] rounded-xl flex flex-col justify-between">
+            <View className="p-4 bg-white rounded-full max-w-[60px]">
                 <MaterialComunityIcons name="cart" size={30} />
               </View>
 
               <View>
                 <Text className="text-white font-outfitBold text-2xl text-wrap">
-                  New
+                  See
                 </Text>
                 <Text className="text-white font-outfitBold text-4xl text-wrap">
-                  Product
+                  History
                 </Text>
               </View>
             </View>
           </View>
           <View className="flex flex-row gap-4 items-center justify-between w-full">
-            <View className="w-1/2 p-5 h-[150px] bg-[#78F3B5] rounded-xl flex flex-col justify-between">
-              <View>
+            <View className="w-1/2 p-5 h-[190px]  bg-[#00F279] rounded-xl flex flex-col justify-between">
+            <View className="p-4 bg-white rounded-full max-w-[60px]">
                 <MaterialComunityIcons name="cart" size={30} />
               </View>
 
               <View>
                 <Text className="text-white font-outfitBold text-2xl text-wrap">
-                  New
+                  See
                 </Text>
                 <Text className="text-white font-outfitBold text-4xl text-wrap">
-                  Product
+                  Order
                 </Text>
               </View>
             </View>
-            <View className="w-1/2 p-5 h-[150px] bg-[#00F279] rounded-xl flex flex-col justify-between">
-              <View>
+            <View className="w-1/2 p-5 h-[190px] bg-[#78F3B5]  rounded-xl flex flex-col justify-between">
+            <View className="p-4 bg-white rounded-full max-w-[60px]">
                 <MaterialComunityIcons name="cart" size={30} />
               </View>
 
               <View>
                 <Text className="text-white font-outfitBold text-2xl text-wrap">
-                  New
+                  See
                 </Text>
                 <Text className="text-white font-outfitBold text-4xl text-wrap">
-                  Product
+                  Profile
                 </Text>
               </View>
             </View>
