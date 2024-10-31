574e9344bf9300627ab6bf579160264a72453a46
Mohammad Anjas Ferdiansyah <anjasferdiansyah123@gmail.com>
2024-10-31 11:00:56 +0700

    feat : Slicing CreatingNewProduct Screen

--- screens/NewProduct.js
+++ screens/NewProduct.js
@@ -1,16 +1,28 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, SafeAreaView } from 'react-native'
 import React from 'react'
 import { TouchableOpacity } from 'react-native'
+import RNPickerSelect from 'react-native-picker-select';
 
-const CompletingRegister = ({navigation}) => {
+const NewProduct = ({navigation}) => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <View className="w-full mt-20 p-10">
-        <Text className="text-5xl font-outfitBold w-full">Completing Register</Text>
+      <SafeAreaView className="w-full p-10">
+        <Text className="text-4xl mt-20 font-outfitBold w-full">Create New Product</Text>
         <View className="flex flex-col gap-4 py-safe-or-12">
         <View className="flex flex-col gap-2">
-        <Text className="font-outfitRegular text-gray-500">Vendor Name</Text>
-        <TextInput className="border-[0.5px] py-2 px-4 rounded-xl border-gray-400 text-xs font-outfitRegular" placeholder='Enter vendor name..'/>
+        <Text className="font-outfitRegular text-gray-500">Product Name</Text>
+        <TextInput className="border-[0.5px] py-2 px-4 rounded-xl border-gray-400 text-xs font-outfitRegular" placeholder='Enter product name..'/>
+        </View>
+        <View className="flex flex-col gap-2">
+        <Text className="font-outfitRegular text-gray-500">Vendor Type</Text>
+        <TouchableOpacity className="border-[0.5px] py-2 px-4 rounded-xl border-gray-400 text-xs font-outfitRegular" >
+        <RNPickerSelect useNativeAndroidPickerStyle={false} items={[
+        { label: 'Football', value: 'football' },
+        { label: 'Baseball', value: 'baseball' },
+        { label: 'Hockey', value: 'hockey' },
+      ]}/>
+        </TouchableOpacity>
+       
         </View>
         <View className="flex flex-col gap-2">
         <Text className="font-outfitRegular text-gray-500">Phone Number</Text>
@@ -25,7 +37,7 @@ const CompletingRegister = ({navigation}) => {
         <TextInput className="border-[0.5px] py-2 px-4 rounded-xl border-gray-400 text-xs font-outfitRegular" placeholder='Enter address detail'
         />
         </View>
-        <TouchableOpacity onPress={() => navigation.navigate("NewProduct")} className="bg-[#00AA55] mx-auto w-[90%] mt-12 items-center justify-center px-8 py-3 rounded-full">
+        <TouchableOpacity onPress={() => navigation.navigate("NewProduct")} className="bg-[#00AA55] mx-auto w-[90%] mt-5 items-center justify-center px-8 py-3 rounded-full">
           <Text className="text-white text-xl font-bold">Register</Text>
         </TouchableOpacity>
         <Text className="text-center text-gray-500 text-sm mt-4">
@@ -38,9 +50,9 @@ const CompletingRegister = ({navigation}) => {
           </Text>
         </Text>
         </View>
-      </View>
+      </SafeAreaView>
     </View>
   )
 }
 
-export default CompletingRegister
+export default NewProduct
