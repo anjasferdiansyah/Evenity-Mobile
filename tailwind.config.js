ef27c7f75088a254d14f0081cd6a0d9d0e7a464d
facronactz <facronactz@gmail.com>
2024-10-31 23:55:03 +0700

    build(tailwind): update content paths and reformat config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}, ./screens/screensUser/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-    fontFamily: {
-      outfitLight : ["Outfit-Light", "sans-serif"],
-      outfitRegular : ["Outfit-Regular", "sans-serif"],
-      outfitMedium : ["Outfit-Medium", "sans-serif"],
-      outfitSemiBold : ["Outfit-SemiBold", "sans-serif"],
-      outfitBold : ["Outfit-Bold", "sans-serif"],
-      outfitExtraBold : ["Outfit-ExtraBold", "sans-serif"],
-      outfitBlack : ["Outfit-Black", "sans-serif"],
-      outfitExtraBlack : ["Outfit-ExtraBlack", "sans-serif"],
+    // NOTE: Update this to include the paths to all of your component files.
+    content: [
+        "./screens/**/*.{js,jsx,ts,tsx}",
+        "./components/**/*.{js,jsx,ts,tsx}",
+        "./screens/screensUser/**/*.{js,jsx,ts,tsx}",
+        "./app/**/*.{js,jsx,ts,tsx}",
+    ],
+    presets: [require("nativewind/preset")],
+    theme: {
+        extend: {}, fontFamily: {
+            outfitLight: ["Outfit-Light", "sans-serif"],
+            outfitRegular: ["Outfit-Regular", "sans-serif"],
+            outfitMedium: ["Outfit-Medium", "sans-serif"],
+            outfitSemiBold: ["Outfit-SemiBold", "sans-serif"],
+            outfitBold: ["Outfit-Bold", "sans-serif"],
+            outfitExtraBold: ["Outfit-ExtraBold", "sans-serif"],
+            outfitBlack: ["Outfit-Black", "sans-serif"],
+            outfitExtraBlack: ["Outfit-ExtraBlack", "sans-serif"],
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
