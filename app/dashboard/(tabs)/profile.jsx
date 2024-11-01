17d4785ab275a51eead89ca487ec1ca92f2adffb
facronactz <facronactz@gmail.com>
2024-11-01 20:53:14 +0700

    feat(profile): add logout functionality

--- app/dashboard/(tabs)/profile.jsx
+++ app/dashboard/(tabs)/profile.jsx
@@ -1,18 +1,21 @@
-import { View, Text } from 'react-native'
+import {Text, TouchableOpacity, View} from 'react-native'
 import React from 'react'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
-import { TouchableOpacity } from 'react-native'
+import {useDispatch} from "react-redux";
+import {logout} from "@/redux/slices/authSlice";
 
 const ProfileScreen = () => {
+    const dispatch = useDispatch()
     return (
         <View className="flex-1 items-center justify-center bg-white">
             <View className="w-full h-[70%] px-10">
                 <View className="flex flex-row justify-between">
                     <TouchableOpacity className="w-10 h-10 items-center justify-center bg-[#00AA55] rounded-full">
-                        <MaterialCommunityIcons name='arrow-left' size={20} color={'white'} />
+                        <MaterialCommunityIcons name='arrow-left' size={20} color={'white'}/>
                     </TouchableOpacity>
-                    <TouchableOpacity className="w-10 h-10 items-center justify-center bg-red-500 rounded-full">
-                        <MaterialCommunityIcons color={'white'} name='exit-to-app' size={20} />
+                    <TouchableOpacity className="w-10 h-10 items-center justify-center bg-red-500 rounded-full"
+                                      onPress={() => dispatch(logout())}>
+                        <MaterialCommunityIcons color={'white'} name='exit-to-app' size={20}/>
                     </TouchableOpacity>
                 </View>
                 <View className="mt-20 border-b border-gray-300 pb-20">
