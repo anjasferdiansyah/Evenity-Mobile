2476c163035d9abb68935dada70a9d74b64e753b
Jose Garda Seta <jose8gard@gmail.com>
2024-11-02 10:44:58 +0700

    [ADD] completing register user

---
+++ app/auth/completing-register-user.jsx
@@ -0,0 +1,105 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  completingRegister,
+  resetError,
+  resetStatus,
+} from "@/redux/slices/authSlice";
+import { router } from "expo-router";
+
+const CompletingRegister = () => {
+  const [userName, setUserName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const { registerData, status, error } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (status === "registered") {
+      dispatch(resetStatus());
+      alert("Registered successfully, please login");
+      router.push("/auth/login");
+    }
+  }, [status]);
+
+  useEffect(() => {
+    if (error) {
+      dispatch(resetError());
+      alert(error);
+    }
+  }, [error]);
+
+  const handleRegister = () => {
+    const newRegisterData = {
+      ...registerData,
+      name: userName,
+      phoneNumber,
+      address,
+    };
+    console.log(newRegisterData);
+    dispatch(completingRegister(newRegisterData));
+  };
+
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <View className="w-full mt-20 p-10">
+        <Text className="text-5xl font-outfitBold w-full">
+          Completing Register
+        </Text>
+        <View className="flex flex-col gap-4 py-safe-or-12">
+          <View className="flex flex-col gap-2">
+            <Text className="font-outfitRegular text-gray-500">
+              Name
+            </Text>
+            <TextInput
+              className="border-[0.5px] py-2 px-4 rounded-xl border-gray-400 text-xs font-outfitRegular"
+              placeholder="Enter vendor name.."
+              onChangeText={setUserName}
+              value={userName}
+            />
+          </View>
+          <View className="flex flex-col gap-2">
+            <Text className="font-outfitRegular text-gray-500">
+              Phone Number
+            </Text>
+            <TextInput
+              onChangeText={setPhoneNumber}
+              value={phoneNumber}
+              className="border-[0.5px] py-2 px-4 rounded-xl border-gray-400 text-xs font-outfitRegular"
+              placeholder="Enter phoneNumber number"
+            />
+          </View>
+          <View className="flex flex-col gap-2">
+            <Text className="font-outfitRegular text-gray-500">
+              Address Detail
+            </Text>
+            <TextInput
+              onChangeText={setAddress}
+              value={address}
+              className="border-[0.5px] py-2 px-4 rounded-xl border-gray-400 text-xs font-outfitRegular"
+              placeholder="Enter product name"
+            />
+          </View>
+          <TouchableOpacity
+            onPress={() => handleRegister()}
+            className="bg-[#00AA55] mx-auto w-[90%] mt-12 items-center justify-center px-8 py-3 rounded-full"
+          >
+            <Text className="text-white text-xl font-bold">Register</Text>
+          </TouchableOpacity>
+          <Text className="text-center text-gray-500 text-sm mt-4">
+            Have an account?{" "}
+            <Text
+              className="text-blue-500"
+              onPress={() => router.push("auth/login")}
+            >
+              Login
+            </Text>
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default CompletingRegister;
