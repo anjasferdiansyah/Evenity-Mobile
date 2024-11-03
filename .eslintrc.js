fb0bd99b66c2c73abc93174389163636c7b62fd8
facronactz <facronactz@gmail.com>
2024-11-04 00:03:13 +0700

    build(deps): add ESLint and related dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "parser": "@babel/eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": 2020,
+        "sourceType": "module",
+        "requireConfigFile": false,
+        "ecmaFeatures": {
+            "jsx": true
+        },
+    },
+    extends: [
+        'expo',
+        "plugin:import-x/recommended",
+        "plugin:react-native/all"
+    ],
+    plugins: ["react-native"],
+    settings: {
+        'import-x/resolver': {
+            "babel-module": {
+                alias: {
+                    '@': '.',
+                },
+            },
+            "react-native": {
+                platform: "both",
+                extensions: ['.js', '.jsx', '.ts', '.tsx', '.ios.js', '.android.js']
+            }
+        },
+        'react': {
+            'version': 'detect'
+        }
+    },
+    env: {
+        'react-native/react-native': true
+    },
+    rules: {
+        'import-x/named': 'off',
+        'import/no-unresolved': 'off'
+    }
+};
