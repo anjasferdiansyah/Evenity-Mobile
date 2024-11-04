58e900f805e466b5f545e651ead596e468fc21a9
Mohammad Anjas Ferdiansyah <anjasferdiansyah123@gmail.com>
2024-11-04 16:24:49 +0700

    [FEAT] Fetching data in ProductScreen Vendor

--- redux/store.js
+++ redux/store.js
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import request_requestDetailSlice from "./slices/request-requestDetail-slice";
 import authSlice from "./slices/authSlice";
 import withdrawHistorySlice from "./slices/withdrawHistorySlice";
-
+import productSlice from "./slices/productSlice";
 
 export default configureStore({
     reducer: {
         auth: authSlice,
         withdrawHistory: withdrawHistorySlice,
     requestDetail: request_requestDetailSlice,
+    product : productSlice
   },
 });
