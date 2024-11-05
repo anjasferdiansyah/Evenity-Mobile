504eb2f59d5947cdf05b98f413bbd98204d71dfe
Jose Garda Seta <jose8gard@gmail.com>
2024-11-05 13:15:56 +0700

    fix page vendor history order

--- components/dashboard/transaction/user.jsx
+++ components/dashboard/transaction/user.jsx
@@ -1,119 +1,232 @@
-import {ScrollView, Text, TouchableOpacity, View} from "react-native";
-import React from "react";
+import {
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  useWindowDimensions,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import AntDesignIcons from "react-native-vector-icons/AntDesign";
 import tailwind from "twrnc";
-import {router} from "expo-router";
+import { router } from "expo-router";
+import { useDispatch, useSelector } from "react-redux";
+import { historyOrderCustomer } from "@/redux/slices/request-requestDetail-slice";
+import Animated, {
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 
 const OrderHistoryUser = () => {
-    const historyItems = [
-        {
-            id: 1,
-            date: "20/12/2024",
-            location: "Malang, Indonesia",
-            status: "Success",
-        },
-        {
-            id: 2,
-            date: "21/12/2024",
-            location: "Jakarta, Indonesia",
-            status: "Failed",
-        },
-        {
-            id: 3,
-            date: "22/12/2024",
-            location: "Surabaya, Indonesia",
-            status: "Success",
-        },
-        {
-            id: 4,
-            date: "23/12/2024",
-            location: "Bandung, Indonesia",
-            status: "Success",
-        },
-        {
-            id: 5,
-            date: "24/12/2024",
-            location: "Bali, Indonesia",
-            status: "Failed",
-        },
-        {
-            id: 6,
-            date: "25/12/2024",
-            location: "Yogyakarta, Indonesia",
-            status: "Success",
-        },
-        {
-            id: 7,
-            date: "26/12/2024",
-            location: "Solo, Indonesia",
-            status: "Success",
-        },
-        {
-            id: 8,
-            date: "27/12/2024",
-            location: "Malang, Indonesia",
-            status: "Failed",
-        },
-        {
-            id: 9,
-            date: "28/12/2024",
-            location: "Jakarta, Indonesia",
-            status: "Success",
-        },
-        {
-            id: 10,
-            date: "29/12/2024",
-            location: "Surabaya, Indonesia",
-            status: "Failed",
-        },
-        {
-            id: 11,
-            date: "30/12/2024",
-            location: "Bandung, Indonesia",
-            status: "Failed",
-        },
-    ];
+  const dispatch = useDispatch();
+  const { isLoading, requestDetail, status, error } = useSelector(
+    (state) => state.requestDetail
+  );
+  const { width } = useWindowDimensions();
 
-    return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <View className="w-full h-full pt-20 px-10">
-                <Text className="text-5xl font-outfitBold">History Orders</Text>
-                <ScrollView style={[tailwind`mt-5`]}>
-                    <View className="list-history">
-                        {historyItems.map((item) => (
-                            <TouchableOpacity
-                                onPress={() => router.push("/dashboard/transaction/detail")}
-                                key={item.id}
-                            >
-                                <View
-                                    className={`flex flex-row justify-between items-center mt-3 p-6 ${
-                                        item.status === "Success" ? "bg-[#00F279]" : "bg-red-500"
-                                    } rounded-full`}
-                                >
-                                    <View>
-                                        <Text className="text-3xl font-outfitBold text-center text-white">
-                                            {item.date}
-                                        </Text>
-                                        <Text className="font-outfitRegular text-center text-white">
-                                            {item.location}
-                                        </Text>
-                                    </View>
+  const [selected, setSelected] = useState("All");
 
-                                    <View className="p-4 bg-white rounded-full">
-                                        <AntDesignIcons
-                                            name="right"
-                                            size={30}
-                                            color={`${item.status === "Success" ? "#00AA55" : "red"}`}
-                                        />
-                                    </View>
-                                </View>
-                            </TouchableOpacity>
-                        ))}
-                    </View>
-                </ScrollView>
-            </View>
+  const slideAnim = useSharedValue(0);
+  const paddingHorizontal = 20;
+  const itemWidth = (width - paddingHorizontal * 2) / 3;
+
+  const handlePress = (item, index) => {
+    setSelected(item);
+    slideAnim.value = withTiming(index * itemWidth, { duration: 300 });
+  };
+
+  const animatedIndicatorStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: slideAnim.value }],
+  }));
+
+  useEffect(() => {
+    dispatch(historyOrderCustomer());
+  }, [dispatch]);
+
+  const historyItems = [
+    {
+      id: 1,
+      date: "20/12/2024",
+      location: "Malang, Indonesia",
+      status: "Success",
+    },
+    {
+      id: 2,
+      date: "21/12/2024",
+      location: "Jakarta, Indonesia",
+      status: "Failed",
+    },
+    {
+      id: 3,
+      date: "22/12/2024",
+      location: "Surabaya, Indonesia",
+      status: "Success",
+    },
+    {
+      id: 4,
+      date: "23/12/2024",
+      location: "Bandung, Indonesia",
+      status: "Success",
+    },
+    {
+      id: 5,
+      date: "24/12/2024",
+      location: "Bali, Indonesia",
+      status: "Failed",
+    },
+    {
+      id: 6,
+      date: "25/12/2024",
+      location: "Yogyakarta, Indonesia",
+      status: "Success",
+    },
+    {
+      id: 7,
+      date: "26/12/2024",
+      location: "Solo, Indonesia",
+      status: "Success",
+    },
+    {
+      id: 8,
+      date: "27/12/2024",
+      location: "Malang, Indonesia",
+      status: "Failed",
+    },
+    {
+      id: 9,
+      date: "28/12/2024",
+      location: "Jakarta, Indonesia",
+      status: "Success",
+    },
+    {
+      id: 10,
+      date: "29/12/2024",
+      location: "Surabaya, Indonesia",
+      status: "Failed",
+    },
+    {
+      id: 11,
+      date: "30/12/2024",
+      location: "Bandung, Indonesia",
+      status: "Failed",
+    },
+  ];
+
+  const filteredItems =
+    selected === "All"
+      ? historyItems
+      : historyItems.filter((item) => item.status === selected);
+
+  return (
+    <View className="flex-1 bg-gray-50">
+      <View className="w-full h-full pt-14 px-6">
+        <View className="flex flex-col items-center">
+          <Text className="text-2xl font-outfitBold text-center text-gray-800 mb-1">
+            Order
+          </Text>
+          <View className="flex flex-col items-center">
+            <Text className="text-4xl font-outfitBold text-center text-[#00AA55]">
+              History
+            </Text>
+            {/* <View className="h-1 bg-[#00AA55] w-24 rounded-full mt-1" /> */}
+          </View>
         </View>
-    );
+
+        <View className="mt-6">
+          <View className="flex flex-row justify-around relative bg-gray-100 p-4 rounded-full">
+            {["All", "Success", "Failed"].map((item, index) => (
+              <TouchableOpacity
+                key={item}
+                onPress={() => handlePress(item, index)}
+                style={{
+                  width: itemWidth,
+                  alignItems: "center",
+                  paddingVertical: 2,
+                }}
+              >
+                <Text
+                  style={{
+                    color: selected === item ? "white" : "black",
+                    fontWeight: "bold",
+                    fontSize: 16,
+                    backgroundColor:
+                      selected === item ? "#00AA55" : "transparent",
+                    paddingHorizontal: 15,
+                    paddingVertical: 5,
+                    borderRadius: 15,
+                  }}
+                >
+                  {item}
+                </Text>
+              </TouchableOpacity>
+            ))}
+
+            <Animated.View
+              style={[
+                animatedIndicatorStyle,
+                {
+                  position: "absolute",
+                  bottom: -2,
+                  left:
+                    selected === "All"
+                      ? 25
+                      : selected === "Success"
+                      ? itemWidth - 109
+                      : itemWidth - 120,
+                  width: itemWidth - 30, // Adjust this value based on your design
+                  height: 3,
+                  backgroundColor: "#00AA55",
+                  borderRadius: 2,
+                },
+              ]}
+            />
+          </View>
+        </View>
+
+        <ScrollView className="mt-6">
+          <View className="list-history space-y-4">
+            {filteredItems.map((item) => (
+              <TouchableOpacity
+                onPress={() => router.push("/dashboard/transaction/detail")}
+                key={item.id}
+                style={{
+                  shadowColor: "#000",
+                  shadowOpacity: 0.2,
+                  shadowOffset: { width: 0, height: 2 },
+                  shadowRadius: 4,
+                  elevation: 4,
+                  padding: 10,
+                }}
+              >
+                <View
+                  className={`flex flex-row justify-between items-center p-5 ${
+                    item.status === "Success" ? "bg-[#DFF7E6]" : "bg-[#FDE4E1]"
+                  } rounded-xl`}
+                >
+                  <View>
+                    <Text className="text-xl font-outfitBold text-gray-800">
+                      {item.date}
+                    </Text>
+                    <Text className="text-sm font-outfitRegular text-gray-500">
+                      {item.location}
+                    </Text>
+                  </View>
+
+                  <View className="p-3 bg-white rounded-full">
+                    <AntDesignIcons
+                      name="right"
+                      size={24}
+                      color={item.status === "Success" ? "#00AA55" : "red"}
+                    />
+                  </View>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </ScrollView>
+      </View>
+    </View>
+  );
 };
 
 export default OrderHistoryUser;
--- components/dashboard/transaction/vendor.jsx
+++ components/dashboard/transaction/vendor.jsx
@@ -1,100 +1,102 @@
-import {FlatList, Text, TouchableOpacity, View} from "react-native";
-import {router} from "expo-router";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
 import AntDesignIcons from "react-native-vector-icons/AntDesign";
-import React, {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {historyOrderCustomer} from "@/redux/slices/request-requestDetail-slice";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { historyOrderCustomer } from "@/redux/slices/request-requestDetail-slice";
 import axios from "axios";
 
 export function OrderHistoryVendor() {
-    const dispatch = useDispatch();
-    const {isLoading, requestDetail, status, error} = useSelector(
-        (state) => state.requestDetail
-    );
+  const dispatch = useDispatch();
+  const { isLoading, requestDetail, status, error } = useSelector(
+    (state) => state.requestDetail
+  );
 
-    useEffect(() => {
-        dispatch(historyOrderCustomer());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(historyOrderCustomer());
+  }, [dispatch]);
 
-    const [userBalance, setUserBalance] = useState(0);
+  const [userBalance, setUserBalance] = useState(0);
 
-    useEffect(() => {
-        const fetchUserBalance = async () => {
-            try {
-                const response = await axios.get('transaction/balance/user/9daa0e0e-db69-4d87-b235-bc32f175f260');
-                const {data } = response.data;
-                setUserBalance(data.amount);
-            } catch (error) {
-                console.error('Error fetching user balance:', error);
-            }
-        }
+  useEffect(() => {
+    const fetchUserBalance = async () => {
+      try {
+        const response = await axios.get(
+          "transaction/balance/user/9daa0e0e-db69-4d87-b235-bc32f175f260"
+        );
+        const { data } = response.data;
+        setUserBalance(data.amount);
+      } catch (error) {
+        console.error("Error fetching user balance:", error);
+      }
+    };
 
-        fetchUserBalance();
-    }, [])
+    fetchUserBalance();
+  }, []);
 
+  const renderItem = ({ item }) => (
+    <View
+      key={item.id}
+      className="flex flex-row justify-between items-center mt-5 p-6 bg-[#00F279] rounded-full"
+    >
+      <View>
+        <Text className="text-3xl font-outfitBold text-center text-white">
+          {item.date}
+          {/* {formatDate(item.date)} */}
+        </Text>
+        <Text className="font-outfitRegular text-center text-white">
+          {item.location}
+        </Text>
+      </View>
+      <TouchableOpacity
+        onPress={() => router.push("dashboard/transaction/detail")}
+        className="p-4 bg-white rounded-full"
+      >
+        <AntDesignIcons name="right" size={30} color={"#00AA55"} />
+      </TouchableOpacity>
+    </View>
+  );
 
-    const renderItem = ({item}) => (
-        <View
-            key={item.id}
-            className="flex flex-row justify-between items-center mt-5 p-6 bg-[#00F279] rounded-full"
-        >
-            <View>
-                <Text className="text-3xl font-outfitBold text-center text-white">
-                    {item.date}
-                    {/* {formatDate(item.date)} */}
-                </Text>
-                <Text className="font-outfitRegular text-center text-white">
-                    {item.location}
-                </Text>
-            </View>
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <View className="w-full h-full pt-20 px-10">
+        <View className="border-b border-gray-400 pb-10">
+          <Text className="text-lg font-outfitBold pb-4 text-gray-500 text-center">
+            Your Active balance
+          </Text>
+          <Text className="text-5xl font-outfitBold pb-4 text-center">
+            Rp. {userBalance}
+          </Text>
+          <View className="flex flex-row justify-center gap-4">
             <TouchableOpacity
-                onPress={() => router.push("dashboard/transaction/detail")}
-                className="p-4 bg-white rounded-full"
+              onPress={() => router.push("dashboard/transaction/withdraw")}
+              className="bg-[#00F279] px-8 py-4 w-[40%] rounded-full"
             >
-                <AntDesignIcons name="right" size={30} color={"#00AA55"}/>
+              <Text className="text-white font-outfitBold text-center">
+                Withdraw
+              </Text>
             </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() =>
+                router.push("dashboard/transaction/withdraw/history")
+              }
+              className="bg-[#00AA55] px-8 py-4 w-[40%] rounded-full"
+            >
+              <Text className="text-white text-center font-outfitBold">
+                History
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-    );
-
-
-    return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <View className="w-full h-full pt-20 px-10">
-                <View className="border-b border-gray-400 pb-10">
-                    <Text className="text-lg font-outfitBold pb-4 text-gray-500 text-center">
-                        Your Active balance
-                    </Text>
-                    <Text className="text-5xl font-outfitBold pb-4 text-center">
-                        Rp. {userBalance}
-                    </Text>
-                    <View className="flex flex-row justify-center gap-4">
-                        <TouchableOpacity
-                            onPress={() => router.push("dashboard/transaction/withdraw")}
-                            className="bg-[#00F279] px-8 py-4 w-[40%] rounded-full"
-                        >
-                            <Text className="text-white font-outfitBold text-center">
-                                Withdraw
-                            </Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            onPress={() => router.push("dashboard/transaction/withdraw/history")}
-                            className="bg-[#00AA55] px-8 py-4 w-[40%] rounded-full"
-                        >
-                            <Text className="text-white text-center font-outfitBold">
-                                History
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                <FlatList
-                    data={requestDetail}
-                    renderItem={renderItem}
-                    keyExtractor={(item) => item.id.toString()}
-                    contentContainerStyle={{
-                        paddingBottom: 10
-                    }}
-                />
-            </View>
-        </View>
-    );
+        <FlatList
+          data={requestDetail}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          contentContainerStyle={{
+            paddingBottom: 10,
+          }}
+        />
+      </View>
+    </View>
+  );
 }
