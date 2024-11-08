c6e60d0f49eeb4422c9dd4fa94b4fca41357a56d
Mohammad Anjas Ferdiansyah <anjasferdiansyah123@gmail.com>
2024-11-08 16:45:14 +0700

    [FIX] Fixing change password, add some validation in make event

--- app/dashboard/make-event/index.jsx
+++ app/dashboard/make-event/index.jsx
@@ -5,43 +5,45 @@ import MakeEventLayout from "@/app/dashboard/make-event/layout";
 import React, {useEffect, useState} from "react";
 import {useDispatch} from "react-redux";
 import {registMakeEvent} from "@/redux/slices/makeEventSlice";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { eventSchema } from "@/helper/validator/schema";
 
 const MakeEventName = () => {
-    const [eventName, setEventName] = useState("");
     const dispatch = useDispatch();
     const [isInputValid, setIsInputValid] = useState(false);
 
-    const handleMakeEvent = () => {
-        if (!isInputValid) {
-            Alert.alert(
-                "Invalid Input",
-                "Please enter a valid event name and description."
-            );
-            return;
+    const {
+        control,
+        register,
+        handleSubmit,
+        setValue,
+        formState: { errors, isValid },
+      } = useForm({
+        resolver: zodResolver(eventSchema),
+        defaultValues : {
+            eventName: "",
+            mode: "onChange",
         }
+      });
 
+    const onSubmit = (data) => {
         dispatch(
             registMakeEvent({
-                name: eventName,
-                // description: description,
+                name: data.eventName
             })
         );
     };
 
-    useEffect(() => {
-        if (eventName.length > 0) {
-            setIsInputValid(true);
-        } else {
-            setIsInputValid(false);
-        }
-    }, [eventName]);
+
+    console.log("Is input valid", isValid )
 
     return (
         <MakeEventLayout
             progress={20}
             nextRoute="description"
-            handleNext={handleMakeEvent}
-            isInputValid={isInputValid}
+            handleNext={handleSubmit(onSubmit)}
+            isInputValid={isValid}
         >
             <View className="px-10" style={tailwind`mt-5`}>
                 <Text className="text-6xl font-outfitSemiBold" style={tailwind``}>
@@ -55,12 +57,23 @@ const MakeEventName = () => {
             <View className="flex flex-col gap-4 w-full mt-12 px-10">
                 <View className="flex flex-col gap-2">
                     <Text className="font-outfitRegular">Event Name</Text>
-                    <TextInput
-                        className="border-[0.5px] py-2 px-4 rounded-xl border-gray-400 text-xs font-outfitLight w-full"
-                        placeholder="Enter your event name"
-                        value={eventName}
-                        onChangeText={setEventName}
+                    <Controller
+                        control={control} // menghubungkan dengan kontrol form
+                        name="eventName" // nama field
+                        render={({ field: { onChange, value } }) => (
+                            <TextInput
+                                className="border-[0.5px] py-2 px-4 rounded-xl border-gray-400 text-xs font-outfitLight w-full"
+                                placeholder="Enter your event name"
+                                value={value}
+                                onChangeText={onChange} // update nilai saat input berubah
+                            />
+                        )}
                     />
+                       {errors.eventName && (
+                        <Text style={tailwind`text-red-500`}>
+                            {errors.eventName.message}
+                        </Text>
+                    )}
                 </View>
                 {/* <View className="flex flex-col gap-2">
           <Text className="font-outfitRegular">Description</Text>
